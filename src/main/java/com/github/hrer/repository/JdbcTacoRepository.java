8cd9e56fa57cfa446d1c04a3b84ee0457978f3d0
hrasiter <hrasiter@gmail.com>
2023-11-09 23:44:21 +0300

    factory keyholder return problem resolved.

--- src/main/java/com/github/hrer/repository/JdbcTacoRepository.java
+++ src/main/java/com/github/hrer/repository/JdbcTacoRepository.java
@@ -43,9 +43,10 @@ public class JdbcTacoRepository implements TacoRepository {
 
 	private long saveTacoInfo(Taco taco) {
 		taco.setCreatedAt(new Date(System.currentTimeMillis()));
-		PreparedStatementCreator psc = new PreparedStatementCreatorFactory(
-				"insert into Taco (name, createdAt) values (?, ?)", Types.VARCHAR, Types.TIMESTAMP)
-						.newPreparedStatementCreator(
+		PreparedStatementCreatorFactory factory = new PreparedStatementCreatorFactory(
+				"insert into Taco (name, createdAt) values (?, ?)", Types.VARCHAR, Types.TIMESTAMP);
+		factory.setReturnGeneratedKeys(true);
+		PreparedStatementCreator psc = factory.newPreparedStatementCreator(
 								Arrays.asList(taco.getName(), new Timestamp(taco.getCreatedAt().getTime())));
 		KeyHolder keyHolder = new GeneratedKeyHolder();
 		int a = jdbc.update(psc, keyHolder);
